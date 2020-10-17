38d9b4b0463f1b74ef34bf2a94b20a6745abefb9
kesheriff <bookkesheriff@gmail.com>
2020-10-18 06:17:18 +0700

    movie

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,60 @@
+class MoviesController < ApplicationController
+    before_action :set_movie, only: [:show, :edit, :update, :destroy]
+    
+    def home
+    end
+    def index
+        @movies = Movie.all
+    end
+
+    def show
+        @movies = Movie.find(params[:id])
+    end
+
+    def new
+        @movie = Movie.new
+    end
+
+    def create
+        @movie = Movie.new(movie_params)
+    
+          if @movie.save
+            #redirect_to movies_path(@movie), :notice => "Movie was successfully created."
+            render "show" 
+          else
+            render "new"
+          end
+        
+    end
+
+    def edit
+        @movies = Movie.find(params[:id])
+    end
+
+    def update
+        
+          if @movie.update(movie_params)
+            render "show"
+          else
+            render "edit"
+          end
+        
+    end
+
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        redirect_to movies_path, :notice => "Movie was successfully destroyed."
+    end
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_movie
+      @movie = Movie.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def movie_params
+      params.require(:movie).permit(:title, :rating, :description, :release_date)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_17_225158) do
+ActiveRecord::Schema.define(version: 2020_10_17_231247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "rating"
+    t.text "description"
+    t.datetime "release_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
