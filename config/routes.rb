73a29159f0f9b31b0186eff9431ac2734ad480f7
kesheriff <bookkesheriff@gmail.com>
2020-10-18 08:08:50 +0700

    omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :reviews,:movies,:tmdb_search
+
+devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  resources :reviews,:movies
   root to: 'movies#index'
+
   get 'search' => 'movie_from_tmdb#index' , as:'search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
