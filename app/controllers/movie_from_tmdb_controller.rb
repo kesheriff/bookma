f6befedc1dc615aede54e277c1390f135f018c77
kesheriff <bookkesheriff@gmail.com>
2020-10-20 01:04:44 +0700

    Tmdb fix

--- app/controllers/movie_from_tmdb_controller.rb
+++ app/controllers/movie_from_tmdb_controller.rb
@@ -6,10 +6,17 @@ class MovieFromTmdbController < ApplicationController
         @search.resource('movie') # determines type of resource
         @search.query(params[:search]) # the query to search against
         @movie_list = @search.fetch
+        @list_class = Array.new
+        
         if (@movie_list)
-          return @movie_list
+          @movie_list.each do |movie|
+            @movie_test = MovieFromTmdb.new(:title => movie['original_title'],:rating => movie['vote_average'],:release_date => movie['release_date'], :description => ' ')
+            @movie_test.save
+            @list_class.push(@movie_test)
+          end
+          return @list_class
         else
-          return @movie_list = []
+          return @list_class = []
         end
     end
 
@@ -17,33 +24,34 @@ class MovieFromTmdbController < ApplicationController
         Tmdb::Api.key("1800a69a4e84911cbb0d843dea9ed085")
         Tmdb::Api.language("de")
         @search = Tmdb::Search.new
+         
         @search.resource('movie') # determines type of resource
         @search.query(params[:search]) # the query to search against
         @movie_list = @search.fetch
         @list_class = Array.new
+        
         if (@movie_list)
           @movie_list.each do |movie|
             @movie_test = MovieFromTmdb.new(:title => movie['original_title'],:rating => movie['vote_average'],:release_date => movie['release_date'], :description => ' ')
             @movie_test.save
             @list_class.push(@movie_test)
           end
-          @movie_list
         else
           @movie_list = []
         end
     end
 
     def edit
-      @movie_test = MovieDb.find(params[:id])
+      @movie_test = MovieFromTmdb.find(params[:id])
     end
     #--------------------------------------------------
     def update
-      @movie_test = MovieDb.find(params[:id])
-      @movie = MovieFromTmdb.new(:title => @movie_test.Movie_Title,:Rating => @movie_test.Rating,:Release_Date => @movie_test.Release_Date,:description => @movie_test.More_Info)
-      if(@movie.save)
-        redirect_to @movie
-      else
-        render 'new'
+      @movie_test = MovieFromTmdb.find(params[:id])
+      @movie = Movie.new(:title => @movie_test.title,:rating => @movie_test.rating,:release_date => @movie_test.release_date,:description => @movie_test.description,:user_id =>current_user.id)
+      if @movie.save
+        redirect_to movies_path
+      else 
+        redirect_to movies_path
       end
     end
 end
