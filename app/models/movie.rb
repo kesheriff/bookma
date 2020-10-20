ab46be28d5cc885a59b8058fdaa4ce128a74a054
kesheriff <bookkesheriff@gmail.com>
2020-10-20 07:45:34 +0700

    validates

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,8 +2,6 @@ class Movie < ApplicationRecord
     belongs_to :user
     has_many :reviews
 
-    validates :title, , presence: true
-    validates :rating, presence: true
-    validates :release_date, presence: true
+    
 
 end
